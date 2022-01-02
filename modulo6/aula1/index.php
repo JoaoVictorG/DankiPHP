4691a14ba125ae6934e71829b13f04ffb53359fe
JoaoVictorG <joaovictorgon36@gmail.com>
2022-01-02 13:11:57 -0300

    array

---
+++ modulo6/aula1/index.php
@@ -0,0 +1,9 @@
+<?php
+    $arr = array('Guilherme', 'Joao',  'Felipe', 'Mario');
+    foreach ($arr as $key => $value) {
+        echo $key;
+        echo '=>';
+        echo $value;
+        echo '<hr>';
+    }
+?>
